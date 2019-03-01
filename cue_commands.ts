e659fdfebdfde8c72707a2a9d434956164d523a4
vbett01 <vincentbett@wr-130-64-189-7.medford.tufts.edu>
2019-02-28 22:24:07 -0500

    fixed bugs?

--- cue_commands.ts
+++ cue_commands.ts
@@ -66,9 +66,9 @@ namespace cue {
     ///////////////////////
 
 
-    let timeFwd :number = -99
-    let timeLftTrn: number = -99
-    let timeRghtTrn: number = -99
+    let timeFwd :number = 0
+    let timeLftTrn: number = 0
+    let timeRghtTrn: number = 0
 
     /**
      * Drive forward the specified distance(cm) at specified speed(cm/s) and then stop.
